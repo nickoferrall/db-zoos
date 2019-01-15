35796b4af2a835ecf0bd851624c40b2cff9ea545
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-15 16:32:25 -0500

    CRUD Complete

--- index.js
+++ index.js
@@ -55,6 +55,18 @@ server.delete('/api/zoos/:id', async (req, res) => {
   }
 });
 
+server.put('/api/zoos/:id', async (req, res) => {
+  try {
+    const changes = req.body;
+    const myUpdate = await db('zoos')
+      .where({ id: req.params.id })
+      .update(changes);
+    res.status(200).json(myUpdate);
+  } catch (error) {
+    res.status(400).json({ errorMessage: 'Unable to update that zoo animal.' });
+  }
+});
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
