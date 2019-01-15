4d17ac02a13a674a43aa89799b7b37bf0f9c3abb
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-15 17:22:06 -0500

    Added CRUD for bears - completed stretch.

--- routes/bearRoutes.js
+++ routes/bearRoutes.js
@@ -29,4 +29,52 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const animalId = await db('bears').where({ id: req.params.id });
+    if (animalId.length === 0) {
+      res.status(404).json({ message: 'ID not found.' });
+    } else {
+      res.status(200).json(animalId);
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Unable to find bear.' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteAnimal = await db('bears')
+      .where({ id: req.params.id })
+      .del();
+    if (!deleteAnimal) {
+      res
+        .status(400)
+        .json({ message: 'This ID does not exist in the database.' });
+    } else {
+      res.status(200).json(deleteAnimal);
+    }
+  } catch (error) {
+    res.status(400).json({ errorMessage: 'Unable to delete that bear.' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const changes = req.body;
+    const myUpdate = await db('bears')
+      .where({ id: req.params.id })
+      .update(changes);
+    if (!myUpdate) {
+      res
+        .status(400)
+        .json({ message: 'This ID does not exist in the database.' });
+    } else {
+      res.status(200).json(myUpdate);
+    }
+  } catch (error) {
+    res.status(400).json({ errorMessage: 'Unable to update that bear.' });
+  }
+});
+
 module.exports = router;
